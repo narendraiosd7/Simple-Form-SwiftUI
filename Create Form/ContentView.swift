47676f04aa0baf9318186c79023dffa26f4e49f7
varalakshmi-iOS <varalakshmi1318@gmail.com>
2020-09-13 10:22:59 +0530

    simple form using Swift UI

---
+++ Create Form/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Create Form
+//
+//  Created by Vadde Narendra on 9/13/20.
+//  Copyright © 2020 Narendra Vadde. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            
+            Section {
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
+            
+            Section {
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
